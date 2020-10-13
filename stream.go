38ccf18e83f4fbad380c944db370f54a5a0c24d0
Prithvipal Singh <prithvirathore99@gmail.com>
2020-10-13 22:04:07 +0530

    added Encode method on Encoder

--- stream.go
+++ stream.go
@@ -1,13 +1,23 @@
 package csvparser
 
-import "io"
+import (
+	"encoding/csv"
+	"io"
+)
 
 // NewEncoder returns a new encoder that writes to w.
 func NewEncoder(w io.Writer) *Encoder {
-	return &Encoder{w: w}
+	return &Encoder{w: csv.NewWriter(w)}
 }
 
 // An Encoder writes cvs values to an output stream.
 type Encoder struct {
-	w io.Writer
+	w *csv.Writer
+	//w io.Writer
+}
+
+// Encode method writes cvs encoding
+func (e Encoder) Encode(i interface{}) {
+	data := Marshal(i)
+	e.w.WriteAll(data)
 }
